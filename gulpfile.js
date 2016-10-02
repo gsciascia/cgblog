68ed306d246c6549d624fab6327f320921b91c18
Gsciascia <info@giuseppesciascia.com>
2016-10-02 20:44:31 +0100

    Created Admin Layout

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,52 @@ require('laravel-elixir-vue');
  |
  */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+
+/**
+ * Copy any needed files.
+ *
+ * Do a 'gulp copyfiles' after bower updates
+ */
+gulp.task("updateFilesBackend", function() {
+
+    gulp.src("vendor/bower_components/AdminLTE/dist/css/**/*")
+        .pipe(gulp.dest("resources/assets/backend/thema/"));
+
+    gulp.src("vendor/bower_components/AdminLTE/dist/js/app.min.js")
+        .pipe(gulp.dest("resources/assets/backend/js/app.min.js"));
+
+    // Copy only plugin that I needs
+
+
+    gulp.src("vendor/bower_components/AdminLTE//bootstrap/**/*")
+        .pipe(gulp.dest("resources/assets/backend/bootstrap/"));
+
+    gulp.src("vendor/bower_components/AdminLTE/plugins/jQuery/**/*/")
+        .pipe(gulp.dest("resources/assets/backend/plugins/jQuery/"));
+
+    gulp.src("vendor/bower_components/AdminLTE/plugins/bootstrap-wysihtml5/**/*")
+        .pipe(gulp.dest("resources/assets/backend/plugins/bootstrap-wysihtml5/"));
+
+
+    gulp.src("vendor/bower_components/AdminLTE/plugins/slimScroll/**/*")
+        .pipe(gulp.dest("resources/assets/backend/plugins/slimScroll/"));
+
+});
+
+elixir(function(mix) {
+    // the source file is for default set to /resource/assets/css/
+    mix.styles([
+        '../backend/bootstrap/css/bootstrap.css',
+        '../backend/thema/AdminLTE.css',
+        '../backend/thema/skins/skin-blue.css'
+
+    ],'public/backend-assets/css/all.css')
+
+
+        .scripts([
+            'plugins/jQuery/jquery-2.2.3.min.js', // Jquery needs first
+            'bootstrap/js/bootstrap.js',
+            'plugins/slimScroll/jquery.slimscroll.js', // Jquery needs first
+            'js/app.min.js'
+        ],'public/backend-assets/js/all.js','./resources/assets/backend/')
 });
